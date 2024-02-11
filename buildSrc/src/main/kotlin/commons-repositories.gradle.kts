21b2adc098518ef542dd1dd694500f624508d415
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-02-11 23:46:23 +0100

    Remove useless dependency.

--- buildSrc/src/main/kotlin/commons-repositories.gradle.kts
+++ buildSrc/src/main/kotlin/commons-repositories.gradle.kts
@@ -7,5 +7,4 @@ repositories {
 
     maven("https://papermc.io/repo/repository/maven-public/") // paper, adventure, velocity
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") // spigot
-    maven("https://repo.panda-lang.org/releases/") // expressible
 }
