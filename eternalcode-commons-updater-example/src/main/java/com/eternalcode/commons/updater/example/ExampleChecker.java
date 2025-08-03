fa95908bfa3e426490c4c5402da08b85b4eb9254
vLuckyyy <vluckyyy.biznes@gmail.com>
2025-08-03 19:25:25 +0200

    Use old eternalcombat version for testing purposes.

--- eternalcode-commons-updater-example/src/main/java/com/eternalcode/commons/updater/example/ExampleChecker.java
+++ eternalcode-commons-updater-example/src/main/java/com/eternalcode/commons/updater/example/ExampleChecker.java
@@ -3,10 +3,13 @@ package com.eternalcode.commons.updater.example;
 import com.eternalcode.commons.updater.UpdateResult;
 
 public class ExampleChecker {
+
+    private static final String OLD_ETERNALCOMBAT_VERSION = "1.3.3";
+
     public static void main(String[] args) {
         ExampleUpdateService service = new ExampleUpdateService();
 
-        UpdateResult modrinthResult = service.checkModrinth("EternalCombat", "2.2.0");
+        UpdateResult modrinthResult = service.checkModrinth("EternalCombat", OLD_ETERNALCOMBAT_VERSION);
         System.out.println("Modrinth update available: " + modrinthResult.isUpdateAvailable());
         if (modrinthResult.isUpdateAvailable()) {
             System.out.println("Latest: " + modrinthResult.latestVersion());
