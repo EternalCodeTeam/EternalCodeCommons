5ac53d6104346b9a1b9c220b6bd91a9dbe47a5a2
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2026-01-17 02:30:18 +0100

    Support SNAPSHOT version checking and update Modrinth filtering logic.

--- buildSrc/src/main/kotlin/commons-publish.gradle.kts
+++ buildSrc/src/main/kotlin/commons-publish.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.eternalcode"
-version = "1.3.1"
+version = "1.3.3-SNAPSHOT"
 
 java {
     withSourcesJar()
@@ -14,16 +14,21 @@ publishing {
     repositories {
         mavenLocal()
 
-        maven {
-            url = uri("https://repo.eternalcode.pl/releases")
+        val mavenUsername = System.getenv("ETERNAL_CODE_MAVEN_USERNAME")
+        val mavenPassword = System.getenv("ETERNAL_CODE_MAVEN_PASSWORD")
 
-            if (version.toString().endsWith("-SNAPSHOT")) {
-                url = uri("https://repo.eternalcode.pl/snapshots")
-            }
+        if (!mavenUsername.isNullOrBlank() && !mavenPassword.isNullOrBlank()) {
+            maven {
+                url = uri("https://repo.eternalcode.pl/releases")
 
-            credentials {
-                username = System.getenv("ETERNAL_CODE_MAVEN_USERNAME")
-                password = System.getenv("ETERNAL_CODE_MAVEN_PASSWORD")
+                if (version.toString().endsWith("-SNAPSHOT")) {
+                    url = uri("https://repo.eternalcode.pl/snapshots")
+                }
+
+                credentials {
+                    username = mavenUsername
+                    password = mavenPassword
+                }
             }
         }
     }
@@ -34,3 +39,4 @@ publishing {
         }
     }
 }
+
