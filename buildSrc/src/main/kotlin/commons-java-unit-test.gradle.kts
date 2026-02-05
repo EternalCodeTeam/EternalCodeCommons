893cc131c29d22eab8ec61a180aa65b559365cff
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2026-02-05 21:09:57 +0100

    Add JUnit platform launcher to testRuntimeOnly

--- buildSrc/src/main/kotlin/commons-java-unit-test.gradle.kts
+++ buildSrc/src/main/kotlin/commons-java-unit-test.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.27.7")
     testImplementation("org.awaitility:awaitility:4.3.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.2")
 }
 
 tasks.getByName<Test>("test") {
